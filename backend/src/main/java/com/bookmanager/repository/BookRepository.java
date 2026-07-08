986758fb8b59a7984f285a2c73451fbdd57412fc
yuzhourui <yuzhourui@szgfjt.com>
2026-07-08 22:24:52 +0800

    Fix: BookRepository missing JpaSpecificationExecutor causing compilation error

--- backend/src/main/java/com/bookmanager/repository/BookRepository.java
+++ backend/src/main/java/com/bookmanager/repository/BookRepository.java
@@ -4,6 +4,7 @@ import com.bookmanager.entity.Book;
 import com.bookmanager.enums.BookStatus;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,7 +12,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT b FROM Book b WHERE b.id = :id")
